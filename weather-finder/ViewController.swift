3149583e1d1f11696437b64e4da37811b4650ebb
Takumi Kimura <shachibou@macbook-pro-7.local>
2018-11-09 17:17:17 +0900

    Initial Commit

---
+++ weather-finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather-finder
+//
+//  Created by Takumi Kimura on 2018/11/09.
+//  Copyright © 2018年 com.takumi0kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
